953d97448cddc6ff5ee99a0c25cdbe9270ef4e1b
Argyrum <Fact_core@outlook.com>
2024-05-18 23:43:26 +0200

    Added GLCM gray levels (cmGL) 10 works better than 8, 12 is too rigid

--- Proves Roger Nou/extractFeaturesGLCM.m
+++ Proves Roger Nou/extractFeaturesGLCM.m
@@ -1,6 +1,6 @@
-function [funcProps, funcCM] = extractFeaturesGLCM(funcImgGs, funcOffset, cmSym, funcProps)
+function [funcProps, funcCM] = extractFeaturesGLCM(funcImgGs, funcOffset, cmSym, funcProps, cmGL)
 
-    funcCM = graycomatrix(funcImgGs, Offset = funcOffset, Symmetric = cmSym);
+    funcCM = graycomatrix(funcImgGs, Offset = funcOffset, Symmetric = cmSym, NumLevels = cmGL);
     
     funcProps = graycoprops(funcCM, funcProps);
     funcProps = cell2mat(struct2cell(funcProps));
